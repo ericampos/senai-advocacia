e6c7e8e09d12c706de4fc23a53f8c8a77b652371
Eric de Oliveira Campos <eric.oliveira.campos@gmail.com>
2024-11-22 09:21:22 -0300

    Adicionando os componentes da página e configurando o app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet
+    , HeaderComponent
+    , CarouselComponent
+    , ContactFormComponent
+    , AboutUsComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
